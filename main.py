6e70deddcd1acb11e9fcf28735b7fbc9145a7cd4
Justin <justinkhlee27@gmail.com>
2023-09-02 13:57:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,71 @@ import json
 import datetime
 import time
 import random
+import re
 
-# Every question you complete gives you a QXP, with multipliers for streaks etc. 
-# It needs a... Scraper. 
-# 'QXP earned: ' 
+#rainbow tier
+#bot v bot functionality 
+
+# Define window height and width
+h, w = 30, 81
+
+def generate_question(difficulty):
+    op_list = ['+', '-', '*', '/']
+    operation = random.choice(op_list)
     
+    # Difficulty
+    if difficulty == 1:
+        operand1, operand2 = random.randint(1, 10), random.randint(1, 10)
+    elif difficulty == 2:
+        operand1, operand2 = random.randint(1, 50), random.randint(1, 50)
+    elif difficulty == 3:
+        operand1, operand2 = random.randint(1, 100), random.randint(1, 100)
+    elif difficulty == 4:
+        operand1, operand2 = random.randint(1, 500), random.randint(1, 500)
+    elif difficulty == 5:
+        operand1, operand2 = random.randint(1, 1000), random.randint(1, 1000)
+    
+    # Handle division to avoid non-integer solutions
+    if operation == '/':
+        operand1 = operand1 * operand2
 
-    # :-( :-| :-) 𝝈 - whil eyou're playing. The sigma is multicoloured
-    # Main loop
-    # approximation game
-    # ranking mode
-    # need a json list of q's and a's under certain categories. 
+    question = f"{operand1} {operation} {operand2} = "
+    
+    if operation == '+':
+        answer = operand1 + operand2
+    elif operation == '-':
+        answer = operand1 - operand2
+    elif operation == '*':
+        answer = operand1 * operand2
+    elif operation == '/':
+        answer = operand1 // operand2  # Integer division
+
+    return question, answer
+    # use eval 
+
+def add_skill_bars(stdscr):
+    # Skill Level (thes ecan lash maybe)
+    # Make this a function, refresh after every session. 
+
+    # Zetamac bar
+    # Get the skill level from the json or something
+    zeta_skill = 13
+    stdscr.addstr(3, 40, "▓" * (zeta_skill-1), curses.color_pair(2))
+    stdscr.addstr(3, 40+zeta_skill-1, "▓", curses.color_pair(2) | curses.A_BLINK)
+    stdscr.addstr(3, 40+zeta_skill, "▒" * (17-zeta_skill), curses.color_pair(2))
+
+
+
+    stdscr.addstr(4, 40, "▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(5, 40, "▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(6, 40, "▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(7, 40, "▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(8, 40, "▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒", curses.color_pair(2))
+
+    stdscr.addstr(11, 40, "▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(12, 40, "▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(13, 40, "▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒", curses.color_pair(2))
+    stdscr.addstr(14, 40, "▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒", curses.color_pair(2))
 
 def draw_box(stdscr, y, x, height, width):
     h_max, w_max = stdscr.getmaxyx()
@@ -38,55 +92,78 @@ def draw_box(stdscr, y, x, height, width):
         stdscr.addch(y, i, curses.ACS_HLINE)
         stdscr.addch(y + height, i, curses.ACS_HLINE)
 
-def draw_menu(stdscr, selected_row_idx):
-    h, w = stdscr.getmaxyx()
-    for idx, row in enumerate(["Zetamac", "24", "Sequences", "Probability"]):
-        x = 2
-        y = idx+4
-        if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, row)
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            stdscr.addstr(y, x, row)
-
-def main(stdscr):
-    # Initialize color support
-    curses.start_color()
+def start_game(stdscr, game_time, difficulty):
+    stdscr.nodelay(True)  # set getch() non-blocking
+    stdscr.addstr(5, 3, f"Starting: {game_time} seconds, difficulty {difficulty}")
+    stdscr.refresh()
+    #countdown(stdscr)
+    time.sleep(1)
+    stdscr.addstr(5, 3, "                                                       ")
+    stdscr.addstr(6, 3, "                                                       ")
+    stdscr.refresh()
     
-    # Initialize color pair (Foreground: White, Background: Black)
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)  
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE) 
+    start_time = time.time()
+    elapsed_time = 0
+    time_left = True
+    score = 0 
 
-    # Apply the color pair to stdscr
-    stdscr.bkgd(' ', curses.color_pair(1))
+    while time_left:
+        question, correct_answer = generate_question(difficulty)
+        stdscr.addstr(6, 3, f"> {question}           ")
+        
+        answer_str = ''
+        while True:
+            stdscr.addstr(5, 3, f"⏲: {int(game_time - elapsed_time)+1}s  ") 
+            elapsed_time = time.time() - start_time
 
-    curses.curs_set(0)  # Make cursor invisible
-    
-    # Screen dimensions
-    h_min, w_min = 30, 80
+            if elapsed_time >= game_time:
+                time_left = False
+                break  # Exit inner while loop
 
-    while True:
-        stdscr.clear()
-        h, w = stdscr.getmaxyx()
-
-        if h < h_min or w < w_min:
-            stdscr.addstr(0, 0, "Please resize the window to at least 30x80.")
-        else:
-            # Code to draw your interface here, e.g., draw_box(stdscr, 0, 0, 2, w - 1)
-            stdscr.addstr(0, 0, "Window resized successfully. Press 'q' to quit.")
-            
-            # Listen for 'q' key to break the loop
             key = stdscr.getch()
-            if key == ord('q'):
-                break
+            
+            if key in [curses.KEY_BACKSPACE, ord('\b'), ord('\x7f')]:
+                if len(answer_str) > 0:
+                    answer_str = answer_str[:-1]
+                    stdscr.addstr(6, 5 + len(question) + len(answer_str), ' ')
+                    stdscr.refresh()
 
-        # Refresh the screen
+            elif key != -1 and len(answer_str)+1 < 10:
+                answer_str += chr(key)
+            
+            stdscr.addstr(6, 5 + len(question), answer_str)
+            stdscr.refresh()
+            
+            # Check whether answer is correct 
+            try:
+                if float(answer_str) == correct_answer:
+                    score += 1
+                    stdscr.refresh()
+                    break  # Go to the next question
+            except ValueError:  # Handle the case where answer_str cannot be converted to float
+                continue
+
+    # print your final score was
+    stdscr.addstr(5, 3, f"⏲: 0s") 
+    stdscr.addstr(6, 3, f"Program finished, score: {score}") 
+    stdscr.refresh()
+    stdscr.nodelay(False)
+    key = stdscr.getch()
+
+def countdown(stdscr):
+    for count in reversed(range(1, 4)):
+        stdscr.addstr(6, 3, f"Game starting in {count}...")
         stdscr.refresh()
+        time.sleep(1)
+    stdscr.refresh()
+    time.sleep(0.4)
+    return 
 
-    h, w = 30, 81
 
+
+def draw_home(stdscr):
+    # Clear Screen
+    stdscr.clear()
     # Draw the square and rectangle beneath the top menu bar
     draw_box(stdscr, 0, 0, 16, w-20-2)
 
@@ -101,29 +178,35 @@ def main(stdscr):
         stdscr.addch(2, i, curses.ACS_HLINE) 
     stdscr.addch(2, 0, curses.ACS_LTEE) 
     stdscr.addch(2, w-20-2, curses.ACS_RTEE) 
+
+    # Add line
+    for i in range(1, w-20-2): 
+        stdscr.addch(10, i, curses.ACS_HLINE) 
+    stdscr.addch(10, 0, curses.ACS_LTEE) 
+    stdscr.addch(10, w-20-2, curses.ACS_RTEE) 
     
-    # Add text
-    # Top bar
-    stdscr.addstr(0, 25, " 𝞼-Quant ")
+    
+    # Titles
+    stdscr.addstr(0, 2, " 𝞼-Quant ", curses.A_STANDOUT)
+    stdscr.addstr(2, 2, " Exercises ")
+    stdscr.addstr(10, 2, " Sims ")
+    stdscr.addstr(17, w//2-4, " Progress ")
+
 
     # Game Names
-    stdscr.addstr(4, 2, "[0] Zetamac")
-    stdscr.addstr(5, 2, "[1] 24")
-    stdscr.addstr(6, 2, "[3] Sequences")
-    stdscr.addstr(7, 2, "[4] Probability")
-    stdscr.addstr(8, 2, "[5] Approximation")
-    stdscr.addstr(9, 2, "[6] Approximation")
-    stdscr.addstr(10, 2, "SIMS")
+    stdscr.addstr(3, 2, "[0] Zetamac")
+    stdscr.addstr(4, 2, "[1] 24")
+    stdscr.addstr(5, 2, "[3] Sequences")
+    stdscr.addstr(6, 2, "[4] Probability")
+    stdscr.addstr(7, 2, "[5] Approximation")
+    stdscr.addstr(8, 2, "[6] Approximation")
+
     stdscr.addstr(11, 2, "[A] Type Speed")
     stdscr.addstr(12, 2, "[B] Live Problems")
     stdscr.addstr(13, 2, "[C] Trading/Gambling")
     stdscr.addstr(14, 2, "[D] Full Interview")
 
-    # Skill Level
-    stdscr.addstr(4, 40, "▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒", curses.color_pair(2))
-    stdscr.addstr(5, 40, "▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒", curses.color_pair(2))
-    stdscr.addstr(6, 40, "▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒", curses.color_pair(2))
-    stdscr.addstr(7, 40, "▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒", curses.color_pair(2))
+    add_skill_bars(stdscr)
 
     for i in range(w-20, w-1):
         for j in range(1,10):
@@ -143,28 +226,192 @@ def main(stdscr):
 
 
         curses.curs_set(0)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    
-    current_row = 0
-    
-    draw_menu(stdscr, current_row)
-    
+
+    # Home Screen Event Logic
     while True:
         key = stdscr.getch()
         
-        if key == curses.KEY_UP and current_row > 0:
-            current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < len(["Zetamac", "24", "Sequences", "Probability"]) - 1:
-            current_row += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:
-            #stdscr.addstr(0, 0, f"You selected '{["Zetamac", "24", "Sequences", "Probability"][current_row]}'")
-            stdscr.refresh()
-            stdscr.getch()
-            break
-            
-        draw_menu(stdscr, current_row)
+        if key == ord('H') or key == ord('h'):
+            continue
+        elif key == ord('0'):
+            return 'game_mode0'
+        elif key == ord('1'):
+            game_mode1(stdscr, h, w)
+        # ... (Add more elif blocks for other game modes) ...
+
         stdscr.refresh()
+
+    return 'home'
+
+def game_mode0(stdscr):
+    presets = {'a': [60, 3], 'b': [120, 2], 'c': [100, 5]}
+    while True:
+        stdscr.clear()
+        stdscr.refresh()
+        stdscr.keypad(True)
+
+        # Draw Layout (GUI) 
+        draw_box(stdscr, 0, 0, h, w-1)
+        for i in range(1, w-1): 
+            stdscr.addch(2, i, curses.ACS_HLINE) 
+        stdscr.addch(2, 0, curses.ACS_LTEE) 
+        stdscr.addch(2, w-1, curses.ACS_RTEE) 
+
+        for i in range(1, w-1): 
+            stdscr.addch(h-3, i, curses.ACS_HLINE) 
+        stdscr.addch(h-3, 0, curses.ACS_LTEE) 
+        stdscr.addch(h-3, w-1, curses.ACS_RTEE) 
+
+        stdscr.addstr(0, 2, " 𝞼-Quant ", curses.A_STANDOUT)
+        stdscr.addstr(0, 13, " Zetamac ", curses.A_STANDOUT)
+        stdscr.addstr(2, 2, " Welcome to zetamac ")
+
+        #draw_box(stdscr, 3, 58, 23, 20)
+        for j in range(2, 27):
+            stdscr.addch(j, 58, curses.ACS_VLINE)
+        stdscr.addch(2, 58, curses.ACS_TTEE) 
+        stdscr.addch(27, 58, curses.ACS_BTEE) 
+
+
+        y, x = 3, 3  # Position of the text field
+        input_str = ""
+        
+        stdscr.addstr(y, x, "> ")
+        
+        while True:
+            # Capture key press
+            key = stdscr.getch()
+            
+            # If 'Enter' is pressed, break the loop
+            if key == ord('\n'):
+                break
+            
+            # Handle backspace
+            elif key in [curses.KEY_BACKSPACE, ord('\b'), ord('\x7f')]:
+                if len(input_str) > 0:
+                    input_str = input_str[:-1]
+                    stdscr.addstr(y, x + 2 + len(input_str), " ")
+                    stdscr.refresh()
+            
+            # Handle other printable characters
+            elif (32 <= key <= 126) and len(input_str) <= 50:
+                input_str += chr(key)
+            
+            # Display the updated input string
+            stdscr.addstr(y, x + 2, input_str)
+            stdscr.refresh()
+        
+        # Do something with input_str here (e.g., analyze or store it)
+        if (input_str == 'home'):
+            return 'home'
+        
+        # Now, match against input_str   
+        pattern = r"run (\d{1,3}) (\d) ?(p?)"
+        match = re.fullmatch(pattern, input_str)
+
+        pattern2 = r"run ([a-f])"
+        match2 = re.fullmatch(pattern2, input_str)
+
+        if (match):
+            # Start game 
+            time_val = int(match.group(1))
+            difficulty_val = int(match.group(2))
+            optional_p = match.group(3)
+            start_game(stdscr, time_val, difficulty_val)
+
+        elif (match2):
+            time_val = presets[match2.group(1)][0]
+            difficulty_val = presets[match2.group(1)][1]
+            optional_p = 'p'
+            start_game(stdscr, time_val, difficulty_val)
+
+        else: 
+            stdscr.addstr(5, 3, f"'{input_str}' is not a valid command")
+            stdscr.addstr(6, 3, "Press ↳ to try again")
+            stdscr.getch()  # Wait for another key press to exit, currently just resets 
+            stdscr.refresh()
+            
+        
+        '''
+        # Add game logic here
+        key = stdscr.getch()
+        stdscr.refresh()
+        if key == ord('H') or key == ord('h'):
+            return 'home'
+        '''
+        
+        # Type: ‘run [time] [difficulty] [p (optional)]’
+        # e.g 'run 120 3 p
+
+    return 'game_mode0'
+
+def game_mode1(stdscr):
+    # use QWAS to select the boxes and then 7-0 to select operations or just mouse 
+    while True:
+        stdscr.clear()
+        draw_box(stdscr, 0, 0, h, w)
+        stdscr.addstr(2, 2, "24. Press 'H' to exit.")
+        # Add game logic here
+        key = stdscr.getch()
+        stdscr.refresh()
+        if key == ord('H') or key == ord('h'):
+            break
+
+# Add more game_mode functions...
+
+
+def main(stdscr):
+    # Initialize color support
+    curses.start_color()
     
+    # Initialize color pair (Foreground: White, Background: Black)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)  
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE) 
+
+    # Apply the color pair to stdscr
+    stdscr.bkgd(' ', curses.color_pair(1))
+
+    # Make cursor invisible
+    curses.curs_set(0)  
+    
+    # Screen dimensions
+    h_min, w_min = h, w
+
+    #get to, tomtxt,addscreen,somethin bout her face ___? mum
+
+    while True:
+        stdscr.clear()
+        h_curr, w_curr = stdscr.getmaxyx()
+
+        if h_curr < h_min or w_curr < w_min:
+            # Show a live read of the screen size 
+            stdscr.addstr(0, 0, "Please resize the window to at least 30x80.")
+        else:
+            # Code to draw your interface here, e.g., draw_box(stdscr, 0, 0, 2, w - 1)
+            stdscr.addstr(0, 0, "Window resized successfully. Press 'q' to quit.")
+            
+            # Listen for 'q' key to break the loop
+            key = stdscr.getch()
+            if key == ord('q') or key == ord('Q'):
+                break
+
+        # Refresh the screen
+        stdscr.refresh()
+
+
+    # CENTRAL LOOP 
+    current_mode = 'home'
+
+    while True:
+        if current_mode == 'home':
+            current_mode = draw_home(stdscr)
+        elif current_mode == 'game_mode0':
+            current_mode = game_mode0(stdscr)
+    draw_home(stdscr)
+    
+if __name__ == "__main__":
+    curses.wrapper(main)
 
     '''
     while True:
@@ -183,8 +430,7 @@ def main(stdscr):
             break
     '''
 
-if __name__ == "__main__":
-    curses.wrapper(main)
+
 
     
     '''
@@ -207,10 +453,6 @@ if __name__ == "__main__":
         elif key == ord('q'):
             break
             '''
-
-if __name__ == "__main__":
-    curses.wrapper(main)
-
 
 
 '''
